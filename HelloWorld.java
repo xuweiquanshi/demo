6906db1387e13eee8ec55511be7a28e881eb7d50
xuweiquanshi <xuweiquanshi@gamil.com>
2020-04-12 21:42:45 +0800

    修改.gitignore文件

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,6 +3,7 @@ public class HelloWorld {
         System.out.println("Hello World1");
 		System.out.println("1");
 		System.out.println("1");
+		System.out.println("1");
 		System.out.println("3");
 		System.out.println("3");
     }
