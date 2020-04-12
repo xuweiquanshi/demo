6a2e547329ee2af3c19da09f86b986d23182aa0f
xuweiquanshi <xuweiquanshi@gamil.com>
2020-04-12 21:48:18 +0800

    修改HEllo

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World1");
-		System.out.println("1");
+		System.out.println("Heldajsd");
 		System.out.println("1");
 		System.out.println("3");
 		System.out.println("3");
+		System.out.println("1");
+		System.out.println("1");
     }
 }
