b6228ac8c52190c2009cad82ad091d530b141847
xuweiquanshi <xuweiquanshi@gamil.com>
2020-04-12 20:38:46 +0800

    Hello World!

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World1");
+		System.out.println("1");
+		System.out.println("3");
+		System.out.println("3");
+    }
+}
